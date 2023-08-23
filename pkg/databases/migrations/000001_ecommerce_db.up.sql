74c68ff9019f163602b2bb4500f750bf51558a0d
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-08-24 03:13:30 +0700

    db migrate

---
+++ pkg/databases/migrations/000001_ecommerce_db.up.sql
@@ -0,0 +1,85 @@
+BEGIN;
+
+-- SET TIME ZONE --
+SET TIME ZONE 'Asia/Bangkok';
+CREATE TABLE "users" (
+  "id" varchar PRIMARY KEY,
+  "username" varchar UNIQUE,
+  "password" varchar,
+  "email" varchar UNIQUE,
+  "role_id" int,
+  "created_at" timestamp,
+  "updated_at" timestamp,
+  "delete_at" timestamp
+);
+
+CREATE TABLE "oauth" (
+  "id" varchar PRIMARY KEY,
+  "user_id" varchar,
+  "access_token" varchar,
+  "refresh_token" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "roles" (
+  "id" int PRIMARY KEY,
+  "title" varchar
+);
+
+CREATE TABLE "products" (
+  "id" varchar PRIMARY KEY,
+  "title" varchar,
+  "description" varchar,
+  "price" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "products_categories" (
+  "id" varchar PRIMARY KEY,
+  "product_id" varchar,
+  "categorie_id" int
+);
+
+CREATE TABLE "categories" (
+  "id" int PRIMARY KEY,
+  "title" varchar UNIQUE
+);
+
+CREATE TABLE "images" (
+  "id" varchar PRIMARY KEY,
+  "filename" varchar,
+  "url" varcher,
+  "product_id" varchar,
+  "create_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "orders" (
+  "id" varchar PRIMARY KEY,
+  "user_id" varchar,
+  "contact" varchar,
+  "address" varchar,
+  "tranfer_file" jsonb,
+  "status" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "products_order" (
+  "id" varchar PRIMARY KEY,
+  "qty" int,
+  "product" jsonb,
+  "order_id" varchar
+);
+
+ALTER TABLE "users" ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("id");
+ALTER TABLE "oauth" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "products_categories" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ALTER TABLE "products_categories" ADD FOREIGN KEY ("categorie_id") REFERENCES "categories" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "products_order" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+COMMIT;
