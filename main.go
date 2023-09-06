3787c8658e5b06fd8562a193e7cb6da55d6fdc0f
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-09-06 17:11:52 +0700

    init func main

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"os"
+
+	"github.com/guatom999/Ecommerce-Go/config"
+	"github.com/guatom999/Ecommerce-Go/modules/servers"
+	"github.com/guatom999/Ecommerce-Go/pkg/databases"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
 func main() {
+	cfg := config.LoadConfig(envPath())
+
+	// fmt.Println(cfg.Db())
+
+	db := databases.DbConnect(cfg.Db())
+
+	defer db.Close()
+
+	// fmt.Println(db)
+
+	servers.NewServer(cfg, db).Start()
 
 }
