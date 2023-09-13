e43e54add7af52a022a778ba2e557d161d18379d
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-09-14 01:40:07 +0700

    create customer usecase

---
+++ modules/users/usersUsecases/userUsecase.go
@@ -0,0 +1,37 @@
+package usersUsecases
+
+import (
+	"github.com/guatom999/Ecommerce-Go/config"
+	"github.com/guatom999/Ecommerce-Go/modules/users"
+	"github.com/guatom999/Ecommerce-Go/modules/users/usersRepositories"
+)
+
+type IUserUsecase interface {
+	InsertCustomer(req *users.UserRegisterReq) (*users.UserPassport, error)
+}
+
+type userUsecase struct {
+	cfg            config.IConfig
+	userRepository usersRepositories.IUserRepository
+}
+
+func UserUsecase(cfg config.IConfig, userRepository usersRepositories.IUserRepository) IUserUsecase {
+	return &userUsecase{
+		cfg,
+		userRepository,
+	}
+}
+
+func (u *userUsecase) InsertCustomer(req *users.UserRegisterReq) (*users.UserPassport, error) {
+
+	if err := req.BcryptHashing(); err != nil {
+		return nil, err
+	}
+
+	result, err := u.userRepository.InsertUser(req, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
