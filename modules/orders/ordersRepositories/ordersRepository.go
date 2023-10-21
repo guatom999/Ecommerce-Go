e8385465cfc75d8ae1363bbcd313b1281d902f9b
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-10-21 11:39:31 +0700

    update query orders

--- modules/orders/ordersRepositories/ordersRepository.go
+++ modules/orders/ordersRepositories/ordersRepository.go
@@ -21,13 +21,40 @@ func (r *orderRepository) FindOneProduct(orderId string) (*orders.Order, error) 
 
 	query := `
 	SELECT 
-		to_jsonb("t")
-	FROM (
+	to_jsonb("t")
+FROM (
+SELECT 
+	"o"."id",
+	"o"."user_id",
+	"o"."transfer_slip",
+	(
 		SELECT 
-		FROM "orders" "o"
-		WHERE "o"."id" = $1
+			array_to_json(array_agg("pt"))
+		FROM (
+			SELECT
+				"spo"."id",
+				"spo"."qty",
+				"spo"."product"
+			FROM "products_orders" "spo"
+			WHERE "spo"."order_id" = "o"."id"
 
-	) AS "t";`
+		) AS "pt"
+	) AS "products",
+	"o"."address",
+	"o"."contact",
+	"o"."status",
+	(
+		SELECT 
+			SUM(COALESCE(("po"."product"->>'price')::FLOAT*("po"."qty")::FLOAT,0))
+		FROM "products_orders" "po"
+		WHERE "po"."order_id" = "o"."id"
+	) AS "total_paid",
+	"o"."created_at",
+	"o"."updated_at"
+FROM "orders" "o"
+WHERE "o"."id" = $1
+
+) AS "t";`
 
 	return nil, nil
 }
